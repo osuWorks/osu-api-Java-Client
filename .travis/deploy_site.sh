fd0901a3b6faafff96dd291eef7b4176ffcb23b0
maxikg <maxmax58@googlemail.com>
2015-05-07 13:42:36 +0200

    Prevent to publish JavaDocs for some cases.

--- .travis/deploy_site.sh
+++ .travis/deploy_site.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-mvn javadoc:aggregate scm-publish:publish-scm --settings $(dirname $0)/settings.xml
+if [ "$TRAVIS_REPO_SLUG" == "osuWorks/osu-api-Java-Client" ] \
+        && [ "$TRAVIS_PULL_REQUEST" == "false" ] \
+        && [ "$TRAVIS_BRANCH" == "master" ]; then
+    mvn javadoc:aggregate scm-publish:publish-scm --settings $(dirname $0)/settings.xml
+fi
