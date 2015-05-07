24dfa5a6080c1c258090e8c3694599e4379553fe
maxikg <maxmax58@googlemail.com>
2015-05-07 21:27:42 +0200

    JavaDoc try #2

--- .travis/deploy_site.sh
+++ .travis/deploy_site.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 if [ "$TRAVIS_REPO_SLUG" == "osuWorks/osu-api-Java-Client" ] \
-        && [ "$TRAVIS_PULL_REQUEST" == "false" ] \
-        && [ "$TRAVIS_BRANCH" == "master" ]; then
-    mvn javadoc:aggregate scm-publish:publish-scm --settings $(dirname $0)/settings.xml
+    && [ "$TRAVIS_PULL_REQUEST" == "false" ] \
+    && [ "$TRAVIS_BRANCH" == "master" ]; then
+
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "travis-ci"
+
+    mvn javadoc:javadoc scm-publish:publish-scm --settings $(dirname $0)/settings.xml
 fi
